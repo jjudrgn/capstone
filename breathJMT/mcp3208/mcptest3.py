6bf33ff93203d45c1755b70beb0d8e7b30e4aa66
root <wndyd9557@gmail.com>
2019-10-29 01:48:52 -0900

    second commit

--- breathJMT/mcp3208/mcptest3.py
+++ breathJMT/mcp3208/mcptest3.py
@@ -1,4 +1,13 @@
 from gpiozero import MCP3208
+import time
 
-adc = MCP3208(0)
-print(adc.value)
+
+adc1 = MCP3208(0)
+adc2 = MCP3208(1)
+
+while(True):
+    print(adc1.value)
+    print('ADC1: {:.2f}'.format(adc1.value))
+    print(adc2.value)
+    print('ADC2: {:.2f}'.format(adc2.value)) 
+    time.sleep(2)
